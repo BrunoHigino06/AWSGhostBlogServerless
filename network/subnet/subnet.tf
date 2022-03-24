8e74fb6896c7ad0e96eb7f6c9b2a505012b2f4be
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-03-24 19:43:16 -0300

    2022.03.24

--- network/subnet/subnet.tf
+++ network/subnet/subnet.tf
@@ -1,113 +1,11 @@
-# ALB Subnet
-
-resource "aws_subnet" "ALBSubnet1" {
+resource "aws_subnet" "subnets" {
+  for_each = var.subnets
+ 
+  availability_zone = each.value["az"]
+  cidr_block = each.value["cidr"]
   vpc_id     = var.vpc_id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "ALBSubnet1"
+    Name = each.value["name"]
   }
-}
-
-output "ALBSubnet1_ID" {
-  value = aws_subnet.ALBSubnet1.id
-}
-
-output "ALBSubnet1_CIDR" {
-  value = aws_subnet.ALBSubnet1.cidr_block
-}
-
-resource "aws_subnet" "ALBSubnet2" {
-  vpc_id     = var.vpc_id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "ALBSubnet2"
-  }
-}
-
-output "ALBSubnet2_ID" {
-  value = aws_subnet.ALBSubnet2.id
-}
-
-output "ALBSubnet2_CIDR" {
-  value = aws_subnet.ALBSubnet2.cidr_block
-}
-
-# FrontEnd Subnet
-
-resource "aws_subnet" "FrontEndSubnet1" {
-  vpc_id     = var.vpc_id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "FrontEndSubnet1"
-  }
-}
-
-output "FrontEndSubnet1_ID" {
-  value = aws_subnet.FrontEndSubnet1.id
-}
-
-output "FrontEndSubnet1_CIDR" {
-  value = aws_subnet.FrontEndSubnet1.cidr_block
-}
-
-resource "aws_subnet" "FrontEndSubnet2" {
-  vpc_id     = var.vpc_id
-  cidr_block = "10.0.4.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "FrontEndSubnet2"
-  }
-}
-
-output "FrontEndSubnet2_ID" {
-  value = aws_subnet.FrontEndSubnet2.id
-}
-
-output "FrontEndSubnet2_CIDR" {
-  value = aws_subnet.FrontEndSubnet2.cidr_block
-}
-
-# DataBase Subnet
-
-resource "aws_subnet" "DataBaseSubnet1" {
-  vpc_id     = var.vpc_id
-  cidr_block = "10.0.5.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "DataBaseSubnet1"
-  }
-}
-
-output "DataBaseSubnet1_ID" {
-  value = aws_subnet.DataBaseSubnet1.id
-}
-
-output "DataBaseSubnet1_CIDR" {
-  value = aws_subnet.DataBaseSubnet1.cidr_block
-}
-
-resource "aws_subnet" "DataBaseSubnet2" {
-  vpc_id     = var.vpc_id
-  cidr_block = "10.0.6.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "DataBaseSubnet2"
-  }
-}
-
-output "DataBaseSubnet2_ID" {
-  value = aws_subnet.DataBaseSubnet2.id
-}
-
-output "DataBaseSubnet2_CIDR" {
-  value = aws_subnet.DataBaseSubnet2.cidr_block
 }
--- network/subnet/vars.tf
+++ network/subnet/vars.tf
@@ -1,1 +1,58 @@
 variable "vpc_id" {}
+
+variable "subnets" {
+    type = map
+    default = {
+
+        # ALB Subnets
+        ALBSubnet1 = {
+            name = "ALBSubnet1"
+            az = "us-east-1a"
+            cidr = "10.0.1.0/24"
+        }
+        ALBSubnet2 = {
+            name = "ALBSubnet2"
+            az = "us-east-1b"
+            cidr = "10.0.2.0/24"
+        }
+        ALBSubnet3 = {
+            name = "ALBSubnet3"
+            az = "us-east-1c"
+            cidr = "10.0.3.0/24"
+        }
+
+        # Frontend Subnets
+        FrontEndSubnet1 = {
+            name = "FrontEndSubnet1"
+            az = "us-east-1a"
+            cidr = "10.0.4.0/24"
+        }
+        FrontEndSubnet2 = {
+            name = "FrontEndSubnet2"
+            az = "us-east-1b"
+            cidr = "10.0.5.0/24"
+        }
+        FrontEndSubnet3 = {
+            name = "FrontEndSubnet3"
+            az = "us-east-1c"
+            cidr = "10.0.6.0/24"
+        }
+
+        # Database subnets
+        DataBaseSubnet1 = {
+            name = "DataBaseSubnet1"
+            az = "us-east-1a"
+            cidr = "10.0.7.0/24"
+        }
+        DataBaseSubnet2 = {
+            name = "DataBaseSubnet2"
+            az = "us-east-1b"
+            cidr = "10.0.8.0/24"
+        }
+        DataBaseSubnet3 = {
+            name = "DataBaseSubnet3"
+            az = "us-east-1c"
+            cidr = "10.0.9.0/24"
+        }         
+    }
+}
