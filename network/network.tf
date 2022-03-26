cab9ea14e491c6fe3eb48e3a553df01480db724b
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-03-26 09:29:33 -0300

    2022.03.26

--- network/network.tf
+++ network/network.tf
@@ -1,14 +1,3 @@
 module "vpc" {
   source = ".\\vpc\\"
 }
-
-module "subnet" {
-  source = ".\\subnet\\"
-
-  # VPC_ID
-  vpc_id = module.vpc.aws_vpc_GhostVPC_ID_output
-
-  depends_on = [
-    module.vpc
-  ]
-}
--- network/routetable/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "vpc_id" {}
-variable "ALBSubnet1_ID" {}
-variable "ALBSubnet2_ID" {}
-variable "FrontEndSubnet1_ID" {}
-variable "FrontEndSubnet2_ID" {}
-variable "DatabaseSubnet1_ID" {}
-variable "DatabaseSubnet2_ID" {}
-variable "InternetGateway_ID" {}
