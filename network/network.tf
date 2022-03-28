a1dd02e0760ef13cd09c565408255b7f21e9f51d
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-03-27 22:02:35 -0300

    2022.03.27

--- network/network.tf
+++ network/network.tf
@@ -1,3 +1,24 @@
 module "vpc" {
-  source = ".\\vpc\\"
+  source = "git::https://github.com/BrunoHigino06/terraform_modules.git//network/vpc"
+
+  vpc = {
+    Name = "GhostVpc"
+    cidr_block = "10.0.0.0/16"
+  }
+}
+
+module "subnet" {
+  source = "git::https://github.com/BrunoHigino06/terraform_modules.git//network/subnet"
+
+  # VPC ID
+  vpc_id = module.vpc.id
+
+  subnet = {
+    name_public = "ALBSubnet"
+    cidr_block_public = ["10.0.1.0/24", "10.0.2.0/24"]
+  }
+
+  depends_on = [
+    module.vpc
+  ]
 }
