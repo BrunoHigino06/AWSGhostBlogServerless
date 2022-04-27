12dcbf292473ea6c73dc4da42ce07f22bd3b9f56
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-04-27 01:12:21 -0300

    2022.04.27

--- network/network.tf
+++ network/network.tf
@@ -19,6 +19,8 @@ module "subnet" {
   subnets_cidr_block = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_az          = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
+  #FrontEnd
+
   depends_on = [
     module.vpc
   ]
