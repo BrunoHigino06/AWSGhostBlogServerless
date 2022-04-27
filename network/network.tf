665f2f4a4bff87274ca5f5db0f177913ca72ddae
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-04-27 01:22:24 -0300

    Update network.tf

--- network/network.tf
+++ network/network.tf
@@ -14,12 +14,9 @@ module "subnet" {
   vpc_id = module.vpc.vpc_id
 
   #SubnetConfiguration
-  #ALB Subnets
-  subnets_names      = ["Alb1", "Alb2", "Alb3"]
-  subnets_cidr_block = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_az          = ["us-east-1a", "us-east-1b", "us-east-1c"]
-
-  #FrontEnd
+  subnets_names      = ["Alb1", "Alb2", "Alb3", "FrontEnd1", "FrontEnd2", "FrontEnd3", "DataBase1", "DataBase2", "DataBase3"]
+  subnets_cidr_block = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24", "10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+  subnet_az          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1a", "us-east-1b", "us-east-1c", "us-east-1a", "us-east-1b", "us-east-1c",]
 
   depends_on = [
     module.vpc
